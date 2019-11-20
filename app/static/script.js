8c7eff551fecbfa7b4f3184e1540fc8e489deb47
Maycon <maycon.abe01@gmail.com>
2019-11-20 20:16:42 -0300

    Site

---
+++ app/static/script.js
@@ -0,0 +1,8 @@
+
+$('#switch').on('click', () => {
+    if ($('#switch').prop('checked')) {
+        $('.wrapper').addClass('dark');
+    } else {
+        $('.wrapper').removeClass('dark');
+    }
+})
